ec484e31985f77340fc4fc8123deca167cf0727e
a96363877 <a96363877@gmail.com>
2025-05-17 19:45:28 +0300

    as

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import "@/app/globals.css"
+import Script from "next/script"
 
 export const metadata = {
   title: "متجري - موقع التسوق الإلكتروني",
@@ -14,6 +15,15 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <body>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=AW-17088682449`} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17088682449');
+          `}
+        </Script>
           {children}
       </body>
     </html>
